562d5408190a6bb63ccf01a39f712731bb9e7e8a
quar17esma <quar17esma@gmail.com>
2019-03-11 12:42:13 +0200

    Complete assignment Binding Validation for Ingredient controller

--- src/main/java/guru/springframework/commands/IngredientCommand.java
+++ src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -3,7 +3,10 @@ package guru.springframework.commands;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
@@ -14,8 +17,16 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class IngredientCommand {
     private String id;
+
     private String recipeId;
+
+    @NotBlank
     private String description;
+
+    @NotNull
+    @Min(1)
     private BigDecimal amount;
+
+    @NotNull
     private UnitOfMeasureCommand uom;
 }
